a11e426989a15e53d5796845f545e199669ddf1d
wiinuk <wii.nukegara@gmail.com>
2022-03-02 09:07:20 +0900

    first commit

---
+++ source/minpoke-friend-qr.user.ts
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Minpoke Friend QR
+// @namespace    https://github.com/wiinuk/minpoke-friend-qr
+// @updateURL    https://github.com/wiinuk/minpoke-friend-qr/raw/master/minpoke-friend-qr.user.js
+// @homepageURL  https://github.com/wiinuk/minpoke-friend-qr
+// @version      0.1
+// @description  Add QR code to friend list
+// @author       Wiinuk
+// @match        https://9db.jp/pokego/data/432/*
+// @match        https://9db.jp/pokemongo/data/4264/*
+// @icon         https://www.google.com/s2/favicons?domain=9db.jp
+// @grant        none
+// ==/UserScript==
+
+import { main } from "./minpoke-friend-qr";
+main();
