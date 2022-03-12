a1052f5eba41db36777aa593753c23be69e33cd3
wiinuk <wii.nukegara@gmail.com>
2022-03-12 12:31:13 +0900

    0.2.3

--- source/minpoke-friend-qr.user.ts
+++ source/minpoke-friend-qr.user.ts
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         Minpoke Friend QR
 // @namespace    https://github.com/wiinuk/minpoke-friend-qr
-// @downloadURL    https://github.com/wiinuk/minpoke-friend-qr/raw/master/minpoke-friend-qr.user.js
+// @downloadURL  https://github.com/wiinuk/minpoke-friend-qr/raw/master/minpoke-friend-qr.user.js
+// @updateURL    https://github.com/wiinuk/minpoke-friend-qr/raw/master/minpoke-friend-qr.user.js
 // @homepageURL  https://github.com/wiinuk/minpoke-friend-qr
-// @version      0.2.2
+// @version      0.2.3
 // @description  Add QR code to friend list
 // @author       Wiinuk
 // @match        https://9db.jp/pokego/data/432*
